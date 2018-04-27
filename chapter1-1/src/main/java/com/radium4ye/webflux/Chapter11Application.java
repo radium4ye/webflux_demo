6ce903bf3fed2b0094e1800c5f0bd083efe98e8d
yewl <yewl@dxy.cn>
2018-04-27 16:39:13 +0800

    整合 Reactor Netty

---
+++ chapter1-1/src/main/java/com/radium4ye/webflux/Chapter11Application.java
@@ -0,0 +1,12 @@
+package com.radium4ye.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter11Application.class, args);
+	}
+}
---
+++ chapter1-1/src/main/java/com/radium4ye/webflux/config/HttpServerConfig.java
@@ -0,0 +1,33 @@
+package com.radium4ye.webflux.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import reactor.ipc.netty.http.server.HttpServer;
+
+
+/**
+ * @author yeweilei
+ */
+@Configuration
+public class HttpServerConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public HttpServer httpServer(RouterFunction<?> routerFunction) {
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(routerFunction);
+        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
+        HttpServer server = HttpServer.create("localhost", Integer.valueOf(environment.getProperty("server.port", "8080")));
+        server.newHandler(adapter);
+        return server;
+    }
+
+
+}
