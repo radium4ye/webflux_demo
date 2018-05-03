4b941ec9e1087507e06623f04b103e875610c404
yewl <yewl@dxy.cn>
2018-05-03 17:40:56 +0800

    采用函数式来映射控制层

---
+++ chapter2-1/src/main/java/com/radium4ye/webflux/handler/HelloHandler.java
@@ -0,0 +1,25 @@
+package com.radium4ye.webflux.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * hello 处理器
+ *
+ * @author radium4ye
+ */
+@Component
+public class HelloHandler {
+
+    /**
+     * 输出一个hello world
+     *
+     * @param request
+     * @return
+     */
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().body(Mono.just("hello world."), String.class);
+    }
+}
